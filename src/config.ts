3e0f82923fb72a14b847cb0b7b855a6ee95f0a60
jean-philippe martel <j-p@jean-philippes-MacBook-Air.local>
2023-04-07 14:39:03 -0400

    added few translation

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Jean-philippe Martel - Web Developer';
+import i18next, { t } from "i18next";
+export const SITE_TITLE = t("site.title");
 export const SITE_DESCRIPTION = 'Jean-philippe Martel is a software developer based in Montreal, Canada. He specializes in building web applications using  latest  and greatest technologies.';
