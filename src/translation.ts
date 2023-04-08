45bf19da750ed34e3d0606a3c03f7df246472b35
jean-philippe martel <j-p@jean-philippes-MacBook-Air.local>
2023-04-08 17:46:41 -0400

    nead some more work on this

---
+++ src/translation.ts
@@ -0,0 +1,58 @@
+export const translation_key = {
+    site: {
+        title: "title",
+        description: "description",
+        button_show_case: "button-show-case",
+        button_repo: "button-repo",
+    },
+    home: {
+        hi_there: "home.hi-there",
+        i_am: "home.i-am",
+        i_title: "home.my-title",
+        i_do: "home.i-do",
+        connect: "home.connect",
+        latest_projects: "home.latest-project",
+
+    },
+    projects: {
+        title: "projects.title",
+        trans: "home.projects.trans.desc",
+        webserv: "home.projects.webserv.desc",
+        Cube3D: "home.projects.Cube3d.desc",
+    },
+    resume: {
+        intro: "resume.intro",
+        educ: "resume.educ",
+        time_line: {
+            high_school: {
+                title: "resume.timeline.highschool.title",
+                subtitle: "resume.timeline.highschool.subtitle",
+
+            },
+            professional: {
+                title: "resume.timeline.professional.title",
+                subtitle: "resume.timeline.professional.subtitle",
+            },
+            covid: {
+                title: "resume.timeline.covid.title",
+                subtitle: "resume.timeline.covid.subtitle",
+            },
+            "school42": {
+                title: "resume.timeline.school42.title",
+                subtitle: "resume.timeline.school42.subtitle",
+            },
+            "zelexio": {
+                title: "resume.timeline.zelexio.title",
+                subtitle: "resume.timeline.zelexio.subtitle",
+
+
+
+
+
+
+            }
+        }
+
+
+
+    } as const;
